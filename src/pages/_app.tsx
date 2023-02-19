f97c4139495f8edf110ca64a72721b8a6e34a4d0
신항민 <tlsgkdals71@naver.com>
2023-02-19 21:13:21 +0900

    Feat Initial setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,73 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useRef } from "react";
+//
+import { NextComponentType } from "next";
+import { AppContext, AppInitialProps, AppProps } from "next/app";
+//
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+//
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import createEmotionCache from "@/utility/createEmotionCache";
+import lightThemeOptions from "@/styles/theme/lightThemeOptions";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => {
+  // @emotion
+  const emotionCache: EmotionCache = createEmotionCache();
+  // @mui
+  const lightTheme = createTheme(lightThemeOptions);
+  // @react-query
+  const clientRef = useRef<QueryClient>();
+
+  if (!clientRef.current) {
+    clientRef.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+      },
+    });
+  }
+
+  return (
+    <>
+      <QueryClientProvider client={clientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={lightTheme}>
+              <CssBaseline />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </CacheProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
+  );
+};
+
+App.getInitialProps = async ({
+  Component,
+  ctx,
+}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps?.(ctx);
+  }
+
+  return { pageProps };
+};
+
+export default App;
+
+// const [queryClient] = useState(() => new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//     },
+//   },
+// }))
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,64 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+//
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../utility/createEmotionCache";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  // 캐치된 객체를 정의
+  const cache = createEmotionCache();
+  // 서버사이드 랜더링 시 할당된 스타일 객체를 스타일 오브젝트 객체에 입혀줄 것
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) => (props) =>
+        <App emotionCache={cache} {...props} />,
+    });
+  /* eslint-enable */
+
+  const initialProps = await Document.getInitialProps(ctx);
+  //
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  //
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
