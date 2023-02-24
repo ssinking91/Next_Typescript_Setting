88e6ca84ef1a0d49f76d76cdb65840608ac4cbf1
신항민 <tlsgkdals71@naver.com>
2023-02-24 15:41:51 +0900

    Feat Mui V5 Setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import { useRef } from "react";
+import { useState } from "react";
 //
-import { NextComponentType } from "next";
-import { AppContext, AppInitialProps, AppProps } from "next/app";
+import { AppProps, AppContext, AppInitialProps } from "next/app";
 //
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
@@ -12,60 +11,51 @@ import createEmotionCache from "@/utility/createEmotionCache";
 import "@/styles/globals.css";
 import muiTheme from "@/styles/theme/muiTheme";
 
-const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
-  Component,
-  pageProps,
-}) => {
-  // @emotion
-  const emotionCache: EmotionCache = createEmotionCache();
-  // @react-query
-  const clientRef = useRef<QueryClient>();
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-  if (!clientRef.current) {
-    clientRef.current = new QueryClient({
-      defaultOptions: {
-        queries: {
-          refetchOnWindowFocus: false,
-        },
-      },
-    });
-  }
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
 
-  return (
-    <>
-      <QueryClientProvider client={clientRef.current}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <CacheProvider value={emotionCache}>
-            <ThemeProvider theme={muiTheme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </CacheProvider>
-        </Hydrate>
-      </QueryClientProvider>
-    </>
-  );
-};
+function App(props: MyAppProps) {
+    //
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-App.getInitialProps = async ({
-  Component,
-  ctx,
-}: AppContext): Promise<AppInitialProps> => {
-  let pageProps = {};
+    // react-query
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            })
+    );
 
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps?.(ctx);
-  }
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <CacheProvider value={emotionCache}>
+                    <ThemeProvider theme={muiTheme}>
+                        <CssBaseline />
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </CacheProvider>
+            </Hydrate>
+        </QueryClientProvider>
+    );
+}
 
-  return { pageProps };
+App.getInitialProps = async ({ Component, ctx }: AppContext): Promise<AppInitialProps> => {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps?.(ctx);
+    }
+
+    return { pageProps };
 };
 
 export default App;
-
-// const [queryClient] = useState(() => new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       refetchOnWindowFocus: false,
-//     },
-//   },
-// }))
